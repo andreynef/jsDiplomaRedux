1cc2b3e24818a04806ba6ca446054e3455e8c35b
Nefedyev Andrey <andreynef@yandex.ru>
2020-10-26 03:42:24 +0400

    kfdjfd

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+
+export const ACCESSKEY = 'Awhepytu0JPZujZW7f97BMriVV8gKVO9_i2cM82Z1YU';
+export const SECRET = '6LfA1BzLZz3Z2_Co9uWJJB4_fkpZvXZAUCxdQEAHP5o';
+export const CALLBACKURL = 'https://redux.nef-an.ru/auth';
+
+
--- src/functions/toAuth.js
+++ src/functions/toAuth.js
@@ -1,21 +1,19 @@
 import Unsplash, {toJson} from 'unsplash-js';
 import localStorageSet from "./localStorageSet";
+import {ACCESSKEY, SECRET, CALLBACKURL} from "../constants"
 
 
 export default function toAuth(accessToken) {
-  // const ACCESSKEY = 'Awhepytu0JPZujZW7f97BMriVV8gKVO9_i2cM82Z1YU';
+  // const ACCESSKEY= 'Awhepytu0JPZujZW7f97BMriVV8gKVO9_i2cM82Z1YU';
   // const SECRET = '6LfA1BzLZz3Z2_Co9uWJJB4_fkpZvXZAUCxdQEAHP5o';
   // const CALLBACKURL = 'https://redux.nef-an.ru/auth';
-  const accessKey = process.env.ACCESSKEY;//ключ прячем в рут (файл .env) + плагин dotenv-webpack в клиентской части конфиг файла. + gitignore. В create-react-app автомат.
-  const secret = process.env.SECRET;
-  const callbackUrl = process.env.CALLBACKURL;
   // const bearerToken = localStorage.accessTokenForUnsplash;//берем из локала. Если нет то устанавливается на null.
-  alert('in toAuth() process.env.ACCESSKEY:', accessKey);
+  alert('in toAuth() import ACCESSKEY:', ACCESSKEY);
 
   const unsplash = new Unsplash({
-    accessKey: accessKey,
-    secret: secret,
-    callbackUrl: callbackUrl,
+    accessKey: ACCESSKEY,
+    secret: SECRET,
+    callbackUrl: CALLBACKURL,
   });
 
   const codeFromUrl = window.location.search.split('code=')[1];// Считываем код из URL
