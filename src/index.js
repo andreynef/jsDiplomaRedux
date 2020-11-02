ab80f65f9f1f89549a78883fd0836720f2460dd4
Nefedyev Andrey <andreynef@yandex.ru>
2020-11-02 16:52:40 +0400

    jdhfjdh

--- src/index.js
+++ src/index.js
@@ -13,13 +13,9 @@ const accessToken = "PtShaxn0gdCA0zVYBUWvUSpfO_nr7WB93_mgRwJ4ITE";
 
 if (!accessToken){
   toAuth();
-}else if (window.location.pathname ==='/') {
-  alert('in if /');
-}else if (window.location.pathname ==='/404') {
-  alert('in 404');
+}else if (window.location.pathname ==='/test') {
+  alert('in test');
   window.location.assign(`${HOMEPAGE}/404`)
-}else if (window.location.pathname !=='/') {
-  alert('in if !/');
 }else if (window.location.pathname ==='/auth') {
   alert('in if /auth');
 }else if (window.location.pathname !=='/auth') {
